191bbe75c54746bcfd26fd5773c2ed6798b073e9
Gareth Senior <gareth.senior@thisisglobal.com>
2015-07-14 21:55:19 +0100

    removed deprecated tests. added tests for the get_response_size method

--- app/test_parser.py
+++ app/test_parser.py
@@ -1,51 +1,10 @@
 import unittest
 from mock import Mock
 from ddt import ddt, data
-from .parser import HTMLParser, HTMLParser2
-
-# test that _request makes an HTTP _request
-# test it stashed the output on the instance
-# test _parse_html() method
+from .parser import HTMLParser
 
 @ddt
 class TestParser(unittest.TestCase):
-	
-	@data(
-		{
-			'mrkup': "<html><body><ul><li>this is simple test</li></ul></body></html>",
-			'spec': { 'text': None },
-			'expect': [{ 'text': 'this is simple test' }]
-		},
-		{
-			'mrkup': "<html><body><ul><li>test1</li><li>test2</li></ul></body></html>",
-			'spec': { 'text': None },
-			'expect': [{ 'text': 'test1' }, { 'text': 'test2' }]
-		},
-		{
-			'mrkup': "<html><body><ul><li id='t1'>test1</li><li id='t2'>test2</li></ul></body></html>",
-			'spec': {
-				'text': None,
-				'attr': 'id'
-			},
-			'expect': [{ 'text': 'test1', 'attr': 't1' }, { 'text': 'test2', 'attr': 't2' }]
-		},
-		{
-			'mrkup': "<html><body><ul><li id='t1' data-id='td1' href='#t1'>test1</li><li id='t2'>test2</li></ul></body></html>",
-			'spec': {
-				'data-id': 'data-id',
-				'id': 'id',
-				'href': 'href'
-			},
-			'expect': [{ 'data-id': 'td1', 'id': 't1', 'href': '#t1' }, { 'id': 't2', 'data-id': '', 'href': '' }]
-		}
-	)
-	def test_html_parser_text(self, data):
-		parser = HTMLParser('', './/ul/li', data['spec'])
-		mock_response = Mock()
-		mock_response.text = data['mrkup']
-		parser.response = mock_response
-		assert parser.parse_html() == data['expect']
-
 
 	@data(
 		{
@@ -80,14 +39,31 @@ class TestParser(unittest.TestCase):
 			'expect': [{ 'data-id': 'td1', 'id': 't1', 'href': '#t1' }, { 'id': 't2', 'data-id': '', 'href': '' }]
 		}
 	)
-	def test_html_cssselector(self, data):
-		parser = HTMLParser2('', False)
+	def test_html_parsing(self, data):
+		parser = HTMLParser('', False)
 		mock_response = Mock()
 		mock_response.text = data['mrkup']
 		parser.response = mock_response
 		assert parser.get_data(data['selector'], data['spec']) == data['expect']
 
-# TODO - test get_response_sieze method
+
+	def test_get_response_size_from_headers(self):
+		parser = HTMLParser('', False)
+		mock_response = Mock()
+		mock_response.headers = { 'content-length': 1234 }
+		parser.response = mock_response
+		assert parser.get_response_size() == 1234
+
+
+	def test_get_response_size_from_content(self):
+		parser = HTMLParser('', False)
+		mock_response = Mock()
+		mock_response.headers = {}
+		mock_response.content = '1234567890'
+		parser.response = mock_response
+		assert parser.get_response_size() == 10
+
+
 
 if __name__ == '__main__':
     unittest.main()
