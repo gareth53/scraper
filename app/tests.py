932666fe57eb82334d4e7323dcd8e83133a89512
Gareth Senior <gareth.senior@thisisglobal.com>
2015-07-14 21:57:08 +0100

    consilidating all tests in a single file - this is only a small app, so that should be fine

--- app/tests.py
+++ app/tests.py
@@ -2,6 +2,7 @@ import unittest
 from mock import Mock
 from ddt import ddt, data
 from .parser import HTMLParser
+from .utils import decimalize
 
 @ddt
 class TestParser(unittest.TestCase):
@@ -63,7 +64,21 @@ class TestParser(unittest.TestCase):
 		parser.response = mock_response
 		assert parser.get_response_size() == 10
 
-
+@ddt
+class TestDecimalizeUtil(unittest.TestCase):
+	@data(
+		(2, "2.00"),
+		(2.5, "2.50"),
+		(2.52, "2.52"),
+		(2.524, "2.52"),
+		(2.525, "2.52"),
+		(20, "20.00"),
+		(295, "295.00"),
+		(9999, "9999.00"),
+		(12345, "12345.00"),
+	)
+	def test_decimalize(self, data):
+		assert decimalize(data[0]) == data[1]
 
 if __name__ == '__main__':
     unittest.main()
