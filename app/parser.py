89a99f12d9b02e7a627cda2e911b03ae97fa332e
Gareth Senior <gareth.senior@thisisglobal.com>
2015-07-14 22:32:43 +0100

    removing unused imports

--- app/parser.py
+++ app/parser.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
-import json
 import requests
+
 
 class HTMLParser(object):
 
--- app/scrape.py
+++ app/scrape.py
@@ -1,6 +1,6 @@
 from .parser import HTMLParser
-from .utils import decimalize
-import re, json
+import re
+import json
 
 ROOT_URL = 'http://www.sainsburys.co.uk/webapp/wcs/stores/servlet/CategoryDisplay?listView=true&orderBy=FAVOURITES_FIRST&parent_category_rn=12518&top_category=12518&langId=44&beginIndex=0&pageSize=20&catalogId=10137&searchTerm=&categoryId=185749&listId=&storeId=10151&promotionId=#langId=44&storeId=10151&catalogId=10137&categoryId=185749&parent_category_rn=12518&top_category=12518&pageSize=20&orderBy=FAVOURITES_FIRST&searchTerm=&beginIndex=0&hideFilters=true'
 
